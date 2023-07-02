526a84c73e6b7dc506e46908daaafc2a0117932c
Ayrat Gusakov <szi6l7cofoow@mail.ru>
2023-07-02 15:56:44 +0300

    1

--- lib/useful_functions/factorial.py
+++ lib/useful_functions/factorial.py
@@ -2,6 +2,10 @@ def factorial(n): # Определяем функцию
     if n == 0 or n == 1: # Базовый случай
         return 1
     else: # Рекуррентный случай
+        print(n)
         return factorial(n-1) * n # Вызываем эту же функцию, но с меньшим аргументом
+    
+# ((((2-1)* 2)* 3)* 4) = 24
 
-print(factorial.__name__)
+if __name__ == "__main__":
+    print(factorial.__name__)
